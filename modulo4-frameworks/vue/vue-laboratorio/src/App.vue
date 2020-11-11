f567b4bab2551fd3cb12dd1187960c8fa6dfa2e2
Pedro Dominguez Aguilera <dominguezaguilera15@gmail.com>
2020-11-11 21:25:04 +0100

    Base vue-laboratorio

---
+++ modulo4-frameworks/vue/vue-laboratorio/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "App",
+});
+</script>
+
+<style>
+html.js-focus-visible {
+  overflow-y: auto;
+}
+</style>
---
+++ modulo4-frameworks/vue/vue-laboratorio/src/sfc.d.ts
@@ -0,0 +1,5 @@
+// Para decirle a TypeScript cómo usar SFC
+declare module "*.vue" {
+  import Vue from "vue";
+  export default Vue;
+}
